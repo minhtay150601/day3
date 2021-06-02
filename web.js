8bac9d92f30d02c85dd4febde44b3cef5184a641
minhtay150601 <84314013+minhtay150601@users.noreply.github.com>
2021-06-02 17:43:14 +0700

    oke nhe

--- web.js
+++ web.js
@@ -1,40 +1,32 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const fs=require('fs')
 
 const app = express()
+var fileName= 'log.txt';
 app.use(bodyparser.urlencoded({extended:false}));
-
-
-app.use((req, res, next)=>{
-    var d = new Date();
-    var content = req.originalUrl + ":" + d.toDateString()
-        + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds(); 
+app.get('/about',(req,res)=>{
+    res.sendFile(__dirname +'/Public/about.html');
+})
+app.get('/',(req,res)=>{
+    
+    res.sendFile(__dirname +'/Public/index.html');
+})
+app.use((req,res,next)=>{
+    var d= new Date();
+    var content = req.originalUrl +  ":" +d.getHours() +" : "+d.getMinutes()+" : "+d.getSeconds()+" : "+d.toDateString();
     console.log(content);
-    fs.appendFileSync(fileName, '\n' + content);
+    // console.log('ban da truy cap '+req.originalUrl);
+    // console.log('Thoi gian truy cap ' +d.getHours() +" : "+d.getMinutes()+" : "+d.getSeconds()+" : "+d.toDateString());
+    fs.appendFileSync(fileName,'\n'+content);
     next();
 })
-
-app.post('/sendUs',(req, res)=>{
+app.post('/sendUs',(req,res)=>{
     var name = req.body.txtName;
     var email = req.body.txtEmail;
-    var country = req.body.country;
-    res.end('xin chao ' + name + " : " + email + " : " + country );  
-  
-
+    var country = req.body.Country
+    res.end('Hell0 ' + name + ' email : '+ email + country);
 })
-
-app.get('/', (req, res)=>
-{
-    //res.end('Hello     World');
-    res.sendFile(__dirname + '/Public/index.html');
-})
-
-app.get('/about' ,(req,res)=>{
-    res.sendFile(__dirname + '/Public/about.html');
-
-})
-
-
-const PORT =5000;
+const PORT= 5000;
 app.listen(process.env.PORT || PORT);
-console.log('Server is running')
+console.log("Server is running")
